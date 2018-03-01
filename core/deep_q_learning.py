Fixed noise gradient issue

core/deep_q_learning.py
@@ -446,10 +446,11 @@ class DQN(QN):
                 state_shape = list(self.env.observation_space.shape)
                 img_height, img_width, nchannels = state_shape
                 if t > 0 and t % self.config.num_adv_iter == 0 and self.config.adv:
-                    print 'Adding adversarial noise'
+		    self.noise_update = 255*self.noise_update/np.max(self.noise_update, axis=(1,2,3), keepdims=True)
+                    #print 'Adding adversarial noise', self.noise_update
                     noise = self.noise_update + self.prev_noise
                     noise_min = np.min(noise, axis=(1,2,3), keepdims=True)
-                    noise_zero - noise - noise_min
+                    noise_zero = noise - noise_min
                     noise_max = np.max(noise_zero, axis=(1,2,3), keepdims=True)
                     noise_scaled = 255*(noise_zero/noise_max)
                     noise = noise_scaled.astype(np.uint8)
@@ -460,10 +461,10 @@ class DQN(QN):
                 self.prev_noise = noise
 
         if self.config.adv:
-            loss_eval, grad_norm_eval, summary, self.noise_update, _ = self.sess.run([self.loss, \
+	    loss_eval, grad_norm_eval, summary, self.noise_update, _ = self.sess.run([self.loss, \
                                 self.grad_norm, self.merged, self.noise_grad, self.train_op], feed_dict=fd)
         else:
-           loss_eval, grad_norm_eval, summary, _ = self.sess.run([self.loss, \
+            loss_eval, grad_norm_eval, summary, _ = self.sess.run([self.loss, \
                                 self.grad_norm, self.merged, self.train_op], feed_dict=fd)
  
 

q2_linear.py
@@ -256,6 +256,7 @@ class Linear(DQN):
 
             # Default to L2 loss
             else: 
+		print 'Here'
                 diff = (self.out_old - self.out_old_pred)
                 batch_losses = 0.5*tf.reduce_sum(diff**2, axis=1)
                 self.lwf_loss = tf.reduce_mean(batch_losses)
@@ -274,8 +275,9 @@ class Linear(DQN):
             self.loss = self.dqn_loss + self.config.lwf_weight*self.lwf_loss
 
             # Used for adversarial update
-            self.noise_grad = tf.gradients(self.lwf_loss, [self.n])[0]
-
+            print 'N is', self.n
+	    self.noise_grad = tf.gradients(self.lwf_loss, [self.the_noise])[0]
+	    self.noise_grad = tf.Print(self.noise_grad, [self.noise_grad])
 
         ##############################################################
         ######################## END YOUR CODE #######################

q3_nature.py
@@ -155,13 +155,13 @@ class NatureQN(Linear):
 			
 			# If we are using noise
 			if self.config.noise:
-
+				self.the_noise = noise
 				# Pass in the noise input through the previously defined common new graph operations
 				# Note the reuse flag
 				with tf.variable_scope('new_' + scope, reuse=True):
 
 					# Common operations for all splits
-					conv1_old = tf.contrib.layers.conv2d(noise, num_outputs=32, kernel_size=8, stride=4, padding='SAME')
+					conv1_old = tf.contrib.layers.conv2d(self.the_noise, num_outputs=32, kernel_size=8, stride=4, padding='SAME')
 					conv2_old = tf.contrib.layers.conv2d(conv1_old, num_outputs=64, kernel_size=4, stride=2, padding='SAME')
 					
 					# These are common layers if we are using 2 or 1 unique layers